7ce4f017053d1dc81033ac2b323aa79fdc45824f
imheonjun <jumong40@naver.com>
2025-07-20 13:47:46 +0900

    refactor extracting url

--- src/main/kotlin/com/emgc/livestreamrecorder/extractor/YoutubeExtractor.kt
+++ src/main/kotlin/com/emgc/livestreamrecorder/extractor/YoutubeExtractor.kt
@@ -1,44 +1,18 @@
 package com.emgc.livestreamrecorder.extractor
 
-import com.emgc.livestreamrecorder.extension.EMPTY
-import com.emgc.livestreamrecorder.util.logger
-import com.google.api.services.youtube.YouTube
-import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Primary
 import org.springframework.stereotype.Component
 
 @Component
-class YoutubeExtractor(
-    private val youtube: YouTube,
-    @Value("\${youtube.google-api.key}") private val apiKey: String,
-) : StreamUrlExtractor {
-    private val log = logger()
+@Primary
+class YoutubeExtractor : StreamUrlExtractor {
 
-    override fun extractUrl(youtubeChannelId: String): String {
-        val response = youtube.search().list("id,snippet")
-            .apply {
-                key = apiKey
-                channelId = youtubeChannelId
-                type = "video"
-                maxResults = 20
-                order = "date"
-            }.execute()
-
-        val liveStreamVideoId = response.items
-            .firstOrNull { it.snippet.liveBroadcastContent == "live" }
-            ?.id
-            ?.videoId
-            .orEmpty()
-
-        if (liveStreamVideoId.isEmpty()) {
-            return String.EMPTY
-        }
-
-        return "$YOUTUBE_BASE_URL$YOUTUBE_WATCH_PATH?$YOUTUBE_V_QUERY_STRING=$liveStreamVideoId"
+    override fun extractUrl(channelName: String): String {
+        return "${YOUTUBE_BASE_URL}/${channelName}/${YOUTUBE_LIVE_PATH}"
     }
 
     companion object {
         private const val YOUTUBE_BASE_URL = "https://www.youtube.com"
-        private const val YOUTUBE_WATCH_PATH = "/watch"
-        private const val YOUTUBE_V_QUERY_STRING = "v"
+        private const val YOUTUBE_LIVE_PATH = "/live"
     }
 }
