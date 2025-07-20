c0f04fb20866ff8c3a292a2e5b216ac5aba2bad5
imheonjun <jumong40@naver.com>
2025-07-20 13:48:51 +0900

    refactor scheduler

--- src/main/kotlin/com/emgc/livestreamrecorder/scheduler/LiveStreamScheduler.kt
+++ src/main/kotlin/com/emgc/livestreamrecorder/scheduler/LiveStreamScheduler.kt
@@ -1,6 +1,7 @@
 package com.emgc.livestreamrecorder.scheduler
 
-import com.emgc.livestreamrecorder.config.Channel
+
+import com.emgc.core.domain.ChannelRepository
 import com.emgc.livestreamrecorder.enums.RecordingStatus
 import com.emgc.livestreamrecorder.extractor.StreamUrlExtractor
 import com.emgc.livestreamrecorder.recorder.StreamRecorder
@@ -12,31 +13,39 @@ import org.springframework.stereotype.Component
 
 @Component
 class LiveStreamScheduler(
-    private val channels: List<Channel>,
     private val streamUrlExtractor: StreamUrlExtractor,
     private val streamRecorder: StreamRecorder,
-    private val ioCoroutineScope: CoroutineScope
+    private val ioCoroutineScope: CoroutineScope,
+    private val channelRepository: ChannelRepository,
 ) {
     private val log = logger()
 
-    @Scheduled(cron = "0 */1 * * * *")
+    @Scheduled(cron = "0 */5 * * * *")
     fun record() {
+        val channels = channelRepository.findAll()
+
         for (channel in channels) {
             if (channel.status == RecordingStatus.RECORDING) {
                 continue
             }
 
-            val url = streamUrlExtractor.extractUrl(channel.channelId)
+            val url = streamUrlExtractor.extractUrl(channel.channelName)
             if (url.isEmpty()) {
                 continue
             }
 
+            channel.record()
+            channelRepository.save(channel)
+
             ioCoroutineScope.launch {
-                log.info("\"${channel.channelName}\"의 방송 녹화를 시작합니다.")
-                channel.record()
-                streamRecorder.record(url, channel.channelName)
-                channel.idle()
-                log.info("\"${channel.channelName}\"의 방송 녹화를 중지합니다.")
+                try {
+                    log.info("\"${channel.channelName}\"의 방송 녹화를 시작합니다.")
+                    streamRecorder.record(url, channel.channelName)
+                    log.info("\"${channel.channelName}\"의 방송 녹화를 중지합니다.")
+                } finally {
+                    channel.idle()
+                    channelRepository.save(channel)
+                }
             }
         }
 
