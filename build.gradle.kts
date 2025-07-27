3345417d2244ea501f8a13b998f46d0bb17200f9
imheonjun <jumong40@naver.com>
2025-07-27 09:52:45 +0900

    add logging configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     runtimeOnly("io.micrometer:micrometer-registry-prometheus")
 
+    // Logging
+    implementation("net.logstash.logback:logstash-logback-encoder:7.3")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
